2bbb6a8c7c6a4c40ff408b04dbd4d6254d7e8fa9
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-06 12:22:09 +0200

    First component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <>
+            <Header text='Some text'/>
+            <div className="container">
+                <h1>My App</h1>
 
-    </div>
-  );
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = ({text}) => {
+
+    return (
+        <header>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    );
+
+}
+
+export default Header;
