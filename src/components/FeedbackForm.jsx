188d5d7cf3882a3c4aa6f94cd14f01fccc14d1ae
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-06 19:42:44 +0200

    22. Added Rating component. Set up functions

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,30 +1,33 @@
 import React, {useState} from 'react';
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 const FeedbackForm = () => {
     const [text, setText] = useState('');
+    const [rating, setRating] = useState(10);
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
 
-    const handleTextChange =(e)=>{
-        if (text === ''){
+    const handleTextChange = (e) => {
+        if (text === '') {
             setBtnDisabled(true)
             setMessage(null)
-        } else if (text !== '' && text.trim().length +2 <= 10){
+        } else if (text !== '' && text.trim().length + 2 <= 10) {
             setMessage('Text must be at least 10 characters.')
             setBtnDisabled(true)
         } else {
             setMessage(null)
             setBtnDisabled(false)
         }
-    setText(e.target.value)
+        setText(e.target.value)
     }
 
     return (
         <Card>
             <form>
                 <h2>How would you rate this servise?</h2>
+                <RatingSelect select={(rating)=>setRating(rating)}/>
                 <div className='input-group'>
                     <input
                         onChange={handleTextChange}
@@ -38,7 +41,6 @@ const FeedbackForm = () => {
             </form>
         </Card>
     );
-
 }
 
 export default FeedbackForm;
