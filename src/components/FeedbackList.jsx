5ba0f47f992f452979dd5b8dde2554f0b6d91520
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-11 23:38:25 +0200

    41. Added isLoading functionality to feedbackList.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -6,22 +6,24 @@ import {useContext} from "react";
 // 6. Import
 
 const FeedbackList = () => {
-    const {feedback} = useContext(feedbackContext)
 
+    const {feedback, isLoading} = useContext(feedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    // if is not loading and there is no feedback or length is 0 then we want to show No Feedback
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p> No feedback Yet</p>
     }
-    return (
+
+    return isLoading ? <h3>Loading...</h3> : (
         <div className='feedback-list'>
             {feedback.map((item) => (
-                    <FeedbackItem
-                        key={item.id}
-                        item={item}
-                    />
+                <FeedbackItem
+                    key={item.id}
+                    item={item}
+                />
             ))}
         </div>
-    );
+    )
 }
 
 export default FeedbackList;
