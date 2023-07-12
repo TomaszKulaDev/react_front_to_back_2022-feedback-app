1ce6f59eee4bc8dc52d4d699b83b0a5483799908
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-12 12:14:30 +0200

    43. Last fix Feedbackcontext

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -34,7 +34,6 @@ export const FeedbackProvider = ({ children }) => {
         })
 
         const data = await response.json()
-
         setFeedback([data, ...feedback])
     }
 
@@ -42,7 +41,6 @@ export const FeedbackProvider = ({ children }) => {
     const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' })
-
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -58,8 +56,6 @@ export const FeedbackProvider = ({ children }) => {
         })
 
         const data = await response.json()
-
-
         setFeedback(feedback.map((item) => (item.id === id ? {...item, ...data} : item)))
 
         setFeedbackEdit({
