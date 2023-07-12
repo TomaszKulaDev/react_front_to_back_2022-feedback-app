87905ed80527c76dd879110421cce9c695c22647
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-12 02:18:04 +0200

    43. Fix in editFeedback, Refactor update feedback & delete func to add data to db.json file.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,86 +1,96 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
-
-export const FeedbackProvider = ({children}) => {
-    const [isLoading, setIsLoading] = useState(true);
+export const FeedbackProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true)
     const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
-        edit: false
-    });
+        edit: false,
+    })
 
     useEffect(() => {
-        fetchData()
+        fetchFeedback()
     }, [])
 
-    const fetchData = async () => {
+    // Fetch feedback
+    const fetchFeedback = async () => {
         const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
         const data = await response.json()
-        console.log(data)
+
         setFeedback(data)
         setIsLoading(false)
-        // Fetch data
     }
-    // Add feedback.
+
+    // Add feedback
     const addFeedback = async (newFeedback) => {
-        const response = await fetch(`http://localhost:5000/feedback`,{
-            method:'POST',
+        const response = await fetch('http://localhost:5000/feedback', {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(newFeedback)
+            body: JSON.stringify(newFeedback),
         })
 
         const data = await response.json()
+
         setFeedback([data, ...feedback])
     }
 
-
-    // Delete feedback. 43
+    // Delete feedback
     const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
-           await fetch(`http://localhost:5000/feedback/${id}`,{method: 'DELETE'})
-            const newFeedback = feedback.filter((item) => item.id !== id)
-            setFeedback(newFeedback)
+            await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' })
+
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
-    //Update feedback item
+    // Update feedback item
     const updateFeedback = async (id, updItem) => {
-        const response = await fetch(`http://localhost:5000/feedback/${id}`,{
+        const response = await fetch(`http://localhost:5000/feedback/${id}`, {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(updItem)
+            body: JSON.stringify(updItem),
         })
 
         const data = await response.json()
-        setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
+
+
+        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...data} : item)))
+
+        setFeedbackEdit({
+            item: {},
+            edit: false,
+        })
     }
 
-
-    // Set item to be updated.
+    // Set item to be updated
     const editFeedback = (item) => {
         setFeedbackEdit({
             item,
-            edit: true
+            edit: true,
         })
     }
-    return <FeedbackContext.Provider
-        value={{
-            feedback,
-            feedbackEdit,
-            isLoading,
-            deleteFeedback,
-            addFeedback,
-            editFeedback,
-            updateFeedback
 
-        }}>{children}</FeedbackContext.Provider>
-
+    return (
+        <FeedbackContext.Provider
+            value={{
+                feedback,
+                feedbackEdit,
+                isLoading,
+                deleteFeedback,
+                addFeedback,
+                editFeedback,
+                updateFeedback,
+            }}
+        >
+            {children}
+        </FeedbackContext.Provider>
+    )
 }
-export default FeedbackContext;
 
+export default FeedbackContext
